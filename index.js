5c6a0686f0f58d18131fdb875e3c37935aa9f4a4
Fossele <fosselewilfried@gmail.com>
2026-03-22 18:43:18 +0100

    basic informational website sompleted

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-http = require("http");
+const express = require('express');
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+
+app.get('/404', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/404.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/contact-me.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
